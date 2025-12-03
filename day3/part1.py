0c8fb70a75e677d30b6cf609443a6aaeaef51546
Kristi Achilleos <krispy2009@gmail.com>
2025-12-03 16:27:30 +0200

    refactor to use itertools

--- day3/part1.py
+++ day3/part1.py
@@ -1,17 +1,18 @@
+import itertools
+
+def find_max_num(line):
+    max_num = 0
+    for combo in itertools.combinations(line.strip(), 2):
+        num = int("".join(combo))
+        if num > max_num:
+            max_num = num
+    # print(max_num)
+    return max_num
+
 with open("input.txt", "r") as file:
     lines = file.readlines()
     total = 0
     for line in lines:
-        max_num = 0
-        for idx, ch in enumerate(line.strip()):
-            for idx2, ch2 in enumerate(line.strip()):
-                if idx2 <= idx:
-                    continue
-                
-                num = int(str(ch) + str(ch2))
-                if num > max_num:
-                    max_num = num
-        total += max_num
+        total += find_max_num(line)
 
 print("total: ", total)
-
